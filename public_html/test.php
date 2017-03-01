2b63c6c3178fb7789b037e515c9de6bcf1dab722
Andrew Pereli <drewpereli@gmail.com>
2017-02-28 22:09:46 -0600

    /Users/Drew/bin/gacp

--- public_html/test.php
+++ public_html/test.php
@@ -3,4 +3,15 @@
 	ORM::configure('mysql:host=localhost;dbname=credit');
 	ORM::configure('username', DB_USER);
 	ORM::configure('password', DB_PASSWORD);
+
+	/*
+	$user = ORM::forTable("users")->create();
+	$user->first_name = "Drew";
+	$user->last_name = "Pereli";
+	$user->email = "A";
+	$user->password_hash = "B";
+	$user->save();
+	*/
+	$user = ORM::forTable("users")->findMany();
+	var_dump($user);
 ?>
