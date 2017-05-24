751b2325aca33dd8d2460a51ae784ca83ea95910
Andrew Pereli <drewpereli@gmail.com>
2017-05-23 19:39:19 -0500

    added automatic logout after 20 minutes of inactivity

--- public_html/logOut.php
+++ public_html/logOut.php
@@ -2,6 +2,9 @@
 	require_once __DIR__ . "/../resources/config.php";
 	requireLogIn();
 	logOut();
+	if (isset($_GET["reason"]) && $_GET["reason"] === "sessionExpired"){
+		$flasher->success = "Logged out due to inactivity.";
+	}
 	header("Location: /");
 	die();
 ?>
